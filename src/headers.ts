4560c6363d45b2f327461301b9dd1ba90d6d19d2
Antti M <antti_rekisteroinnit@hotmail.com>
2022-09-12 21:14:02 +0300

    Improved the headers code a bit by splitting the functions into smaller chunks.

--- src/headers.ts
+++ src/headers.ts
@@ -6,35 +6,46 @@ export interface Header {
 
 export interface HeaderParameter {
     parameterName: string,
-    parameterValue: string
+    parameterValue: string,
 }
 
 export function parseHeaderLine(headerLine: string): Header[] {
-    // Matches the header name (alphabet and dash characters), and one or more header values with zero or more parameters
-    const headerMatches = headerLine.match(/([A-Za-z-]+)\s*:\s*([\w\s\-;,=<>@:.\/]*[\w\-<>@:.])/);
-    if (!headerMatches)
+    const headerNameAndValues = matchHeaderLine(headerLine);
+    if (!headerNameAndValues)
         throw new Error('Invalid header line ' + headerLine);
 
-    const headerName = headerMatches[1];
-    const headerValueParts = headerMatches[2].split(',').map(part => part.trim());
-    const parsedHeaders: Header[] = [];
-    for (const headerValuePart of headerValueParts) {
-        const headerValueMatches = headerValuePart.match(/([\w\-<>@:.\/]*(?:\s*[\w\-<>@:.\/]+)*)\s*(?:;\s*([\w-]+)=([\w@<>\-:.]+))*/);
-        if (!headerValueMatches)
-            throw new Error('Could not parse header value from ' + headerValuePart);
-
-        const headerValue = headerValueMatches[1];
-        const parameters: HeaderParameter[] = [];
-        for (let i = 2; i < headerValueMatches.length; i += 2) {
-            const parameterName = headerValueMatches[i];
-            const parameterValue = headerValueMatches[i + 1];
-            if (!parameterName || !parameterValue)
-                continue;
-            parameters.push({ parameterName,  parameterValue });
-        }
-        parsedHeaders.push({fieldName: headerName, fieldValue: headerValue, parameters: parameters || undefined});
-    }
-
-    return parsedHeaders
+    const headerName = headerNameAndValues[1];
+    const headerValues = headerNameAndValues[2].split(',').map(part => part.trim());
+    return headerValues.map(headerVal => buildSingleHeader(headerName, headerVal));
 }
 
+function buildSingleHeader(headerName: string, headerValue: string): Header {
+    const fieldValueAndParams = matchHeaderValue(headerValue);
+    if (!fieldValueAndParams)
+        throw new Error('Could not parse header value from ' + headerValue);
+
+    const fieldValue = fieldValueAndParams[1];
+    const parameters: HeaderParameter[] = [];
+    for (let i = 2; i < fieldValueAndParams.length; i += 2) {
+        const parameterName = fieldValueAndParams[i];
+        const parameterValue = fieldValueAndParams[i + 1];
+        if (!parameterName || !parameterValue)
+            continue;
+        parameters.push({ parameterName, parameterValue });
+    }
+    return {
+        fieldName: headerName,
+        fieldValue: fieldValue,
+        parameters: parameters || undefined
+    };
+}
+
+function matchHeaderLine(headerLine: string) {
+    // Matches the field name and the entire field value, including potentially multiple header values with parameters.
+    return headerLine.match(/([A-Za-z-]+)\s*:\s*([\w\s\-;,=<>@:.\/]*[\w\-<>@:.])/);
+}
+
+function matchHeaderValue(headerValue: string) {
+    // Matches the header value, potentially with whitespace in the middle, followed by parameters.
+    return headerValue.match(/([\w\-<>@:.\/]*(?:\s*[\w\-<>@:.\/]+)*)\s*(?:;\s*([\w-]+)=([\w@<>\-:.]+))*/);
+}
